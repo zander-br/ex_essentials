4f142780470fb628e9a83d6a41c73e12bd967b35
Anderson Santos <anderson.feitoza.santos@gmail.com>
2025-05-16 16:51:16 -0300

    test: should return a conn with state :unset when the data is successfully validated

---
+++ test/web/plugs/request_validator_test.exs
@@ -0,0 +1,46 @@
+defmodule Utilex.Web.Plugs.RequestValidatorTest do
+  use ExUnit.Case
+
+  import Plug.Conn
+  import Plug.Test
+
+  alias Utilex.Web.Plugs.RequestValidator
+
+  defmodule Users do
+    use Ecto.Schema
+
+    import Ecto.Changeset
+
+    alias __MODULE__
+
+    schema "users" do
+      field(:name, :string)
+      field(:email, :string)
+    end
+
+    def create(params) do
+      %Users{}
+      |> cast(params, ~w(name email)a)
+      |> validate_required(~w(name email)a)
+    end
+  end
+
+  describe "call/2" do
+    setup do
+      opts = RequestValidator.init(validator: Users)
+      params = %{"name" => "Joe Doe", "email" => "joe.doe@mail.com"}
+      %{opts: opts, params: params}
+    end
+
+    test "should return a conn with state :unset when the data is successfully validated",
+         %{opts: opts, params: valid_params} do
+      conn =
+        conn(:post, "/api/users", valid_params)
+        |> put_req_header("content-type", "application/json")
+        |> put_private(:phoenix_action, :create)
+        |> RequestValidator.call(opts)
+
+      assert conn.state == :unset
+    end
+  end
+end
