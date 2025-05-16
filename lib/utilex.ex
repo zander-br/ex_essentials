385d87f6434f40c13a681f7094a0d34104d609b0
Anderson Santos <anderson.feitoza.santos@gmail.com>
2025-05-16 17:47:55 -0300

    refactor: remove unused code

--- lib/utilex.ex
+++ lib/utilex.ex
@@ -1,18 +1,2 @@
 defmodule Utilex do
-  @moduledoc """
-  Documentation for `Utilex`.
-  """
-
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> Utilex.hello()
-      :world
-
-  """
-  def hello do
-    :world
-  end
 end
--- test/utilex_test.exs
+++
@@ -1,8 +0,0 @@
-defmodule UtilexTest do
-  use ExUnit.Case
-  doctest Utilex
-
-  test "greets the world" do
-    assert Utilex.hello() == :world
-  end
-end
