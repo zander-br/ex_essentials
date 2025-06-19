e4c523cad85febb3f195e6bcd40cf440ba33cbc5
Anderson Santos <anderson.feitoza.santos@gmail.com>
2025-06-19 14:33:44 -0300

    feat: add new version

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule ExEssentials.MixProject do
   def project do
     [
       app: :ex_essentials,
-      version: "0.1.0",
+      version: "0.2.0",
       elixir: "~> 1.18",
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
